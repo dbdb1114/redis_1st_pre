633f6f2c256e64647fc71444fbeb9787f5b4ba82
유정현 <dbdb1114@naver.com>
2025-01-05 09:25:40 +0900

    feat: OrderService 동시성 처리 추가
    productDatabase ConcurrentHashMap 변경 및 order메소드 compute() 사용

--- redis_1st_pre/src/main/java/com/concurrent/OrderService.java
+++ redis_1st_pre/src/main/java/com/concurrent/OrderService.java
@@ -2,10 +2,11 @@ package com.concurrent;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class OrderService {
     // 상품 DB
-    private final Map<String, Integer> productDatabase = new HashMap<>();
+    private final ConcurrentHashMap<String, Integer> productDatabase = new ConcurrentHashMap<>();
     // 가장 최근 주문 정보를 저장하는 DB
     private final Map<String, OrderInfo> latestOrderDatabase = new HashMap<>();
 
@@ -18,21 +19,21 @@ public class OrderService {
 
     // 주문 처리 메서드
     public void order(String productName, int amount) {
-        Integer currentStock = productDatabase.getOrDefault(productName, 0);
-
         try {
             Thread.sleep(1); // 동시성 이슈 유발을 위한 인위적 지연
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
-
-        if (currentStock >= amount) {
-            System.out.println("Current Thread : " + Thread.currentThread().getName() +
-                    " - CurrentStock : " + currentStock + " - Order : " + amount);
-            productDatabase.put(productName, currentStock - amount);
-            latestOrderDatabase.put(productName, new OrderInfo(productName, amount, System.currentTimeMillis()));
-        }
+        productDatabase.compute(productName,(key, currentStock)->{ // 원자적 연산처리
+            if (currentStock >= amount) {
+                System.out.printf("%s 주문 정보: \n\t %s: 1건 ([%d])\n", Thread.currentThread().getName().substring(7), productName, amount);
+                latestOrderDatabase.put(productName, new OrderInfo(productName, amount, System.currentTimeMillis()));
+                return currentStock - amount;
+            } else {
+                return currentStock;
+            }
+        });
     }
 
     // 재고 조회
