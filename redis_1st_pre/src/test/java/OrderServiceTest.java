f4943381d00fccd4969735216a3a0e1fbfea2781
유정현 <dbdb1114@naver.com>
2025-01-05 09:28:53 +0900

    Feat: orderWithSynchronized 사용한 테스트 추가

--- redis_1st_pre/src/test/java/OrderServiceTest.java
+++ redis_1st_pre/src/test/java/OrderServiceTest.java
@@ -49,5 +49,42 @@ class OrderServiceTest {
         assertEquals(expectedStock, actualStock, "재고 불일치 보완완료!");
     }
 
+
+    @Test
+    @DisplayName("2. synchronized 사용")
+    void testConcurrentOrdersCauseStockMismatch2() throws InterruptedException {
+        String productName = "apple";
+        int initialStock = service.getStock(productName);
+
+        int orderAmount = 8;
+        int threadCount = 100;
+
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // 각 스레드에서 주문을 수행하는 작업 생성
+        for (int i = 0; i < threadCount; i++) {
+            executor.execute(() -> {
+                try {
+                    service.orderWithSynchronized(productName, orderAmount);
+                } finally {
+                    latch.countDown(); // 작업 완료 후 카운트 감소
+                }
+            });
+        }
+
+        // 모든 스레드가 작업을 완료할 때까지 대기
+        latch.await();
+        executor.shutdown();
+
+        // 최종 재고 값 확인
+        int expectedStock = initialStock % orderAmount;
+        int actualStock = service.getStock(productName);
+
+        System.out.println("Expected Stock: " + expectedStock + ", Actual Stock: " + actualStock);
+
+        // 동시성 이슈로 인해 재고가 맞지 않는 경우를 확인
+        assertEquals(expectedStock, actualStock, "재고 불일치 보완완료!");
+    }
 }
 
